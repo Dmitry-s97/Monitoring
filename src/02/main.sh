48ccb47fcaf5bc265817118f35d248c24e918260
Dmitry <d_igorevish_04@mail.ru>
2023-10-06 22:23:57 +0300

    5

--- src/02/main.sh
+++ src/02/main.sh
@@ -10,4 +10,9 @@ echo `cat /proc/uptime | awk '{print $1}'`
 VAR=`ip a | grep 'dynamic enp0s3' | awk '{print $2}'`
 echo $VAR | sed 's/...$//'
 echo `ipcalc $VAR | grep Netmask | awk '{print $2}'`
-echo `ip r | grep default | awk '{print $3}'` 
+echo `ip r | grep default | awk '{print $3}'`
+VARTOTAL=`grep MemTotal /proc/meminfo` 
+VARFREE=`grep MemFree /proc/meminfo`
+echo $VARTOTAL | awk '{printf("%.3f %s\n", $2/1024^2, $3="GB")}'
+echo `free | grep Mem | awk '{printf("%.3f %s\n", $3/1024^2, $4="GB")}'`
+echo $VARFREE | awk '{printf("%.3f %s\n", $2/1024^2, $3="GB")}'
